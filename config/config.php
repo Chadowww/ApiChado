eb446707a89b09bf9a5aa4508627c865a051e4fc
chado <a.sale@outlook.fr>
2023-10-26 15:12:08 +0200

    Fix: Update variable

--- config/config.php
+++ config/config.php
@@ -1,9 +1,10 @@
 <?php
 
-define('DB_USER', getenv('DB_USER') ? getenv('DB_USER') : $_ENV['APP_DB_USER']);
-define('DB_PASSWORD', getenv('DB_PASSWORD') ? getenv('DB_PASSWORD') : $_ENV['APP_DB_PASSWORD']);
-define('DB_HOST', getenv('DB_HOST') ? getenv('DB_HOST') : $_ENV['APP_DB_HOST']);
-define('DB_NAME', getenv('DB_NAME') ? getenv('DB_NAME') : $_ENV['APP_DB_NAME']);
+//Model (for connexion data, see unversionned db.php)
+define('DB_USER', getenv('DB_USER') ? getenv('DB_USER') : APP_DB_USER);
+define('DB_PASSWORD', getenv('DB_PASSWORD') ? getenv('DB_PASSWORD') : APP_DB_PASSWORD);
+define('DB_HOST', getenv('DB_HOST') ? getenv('DB_HOST') : APP_DB_HOST);
+define('DB_NAME', getenv('DB_NAME') ? getenv('DB_NAME') : APP_DB_NAME);
 
 //View
 define('APP_VIEW_PATH', __DIR__ . '/../src/View/');
