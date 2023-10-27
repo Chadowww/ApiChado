a027a4efc666307acff23af0b16239d0ee47d7ed
chado <a.sale@outlook.fr>
2023-10-27 09:39:34 +0200

    Feat: ADD update() method & Update create() to get parameters in route

--- src/Controller/JobOfferController.php
+++ src/Controller/JobOfferController.php
@@ -6,6 +6,7 @@ use App\Entity\JobOffer;
 use App\Repository\JobOfferRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Serializer\SerializerInterface;
 
 class JobOfferController extends AbstractController
@@ -25,14 +26,14 @@ class JobOfferController extends AbstractController
 //        return $this->json($jobOffers);
 //    }
 
-    public function create(): JsonResponse
+    public function create(Request $request): JsonResponse
     {
         $jobOffer = new JobOffer();
-        $jobOffer->setTitle('Développeur PHP');
-        $jobOffer->setDescription('Développeur PHP/Symfony junior qui roxxe du poney');
-        $jobOffer->setCity('Paris');
-        $jobOffer->setSalaryMin(30000);
-        $jobOffer->setSalaryMax(40000);
+        $jobOffer->setTitle($request->get('title'));
+        $jobOffer->setDescription($request->get('description'));
+        $jobOffer->setCity($request->get('city'));
+        $jobOffer->setSalaryMin($request->get('salaryMin'));
+        $jobOffer->setSalaryMax($request->get('salaryMax'));
 
         if ($this->jobOfferRepository->create($jobOffer)) {
             return new JsonResponse('Created', 201);
@@ -48,10 +49,20 @@ class JobOfferController extends AbstractController
         return new JsonResponse($jobOfferJson, 200, [], true);
     }
 
-//    public function update(int $id)
-//    {
-//        $this->jobOfferRepository->update($id);
-//    }
+    public function update(int $id, Request$request)
+    {
+        $jobOffer = $this->jobOfferRepository->read($id);
+        $jobOffer->setTitle($request->get('title'));
+        $jobOffer->setDescription($request->get('description'));
+        $jobOffer->setCity($request->get('city'));
+        $jobOffer->setSalaryMin($request->get('salaryMin'));
+        $jobOffer->setSalaryMax($request->get('salaryMax'));
+
+        if ($this->jobOfferRepository->update($jobOffer)) {
+            return new JsonResponse('Updated', 200);
+        }
+        return new JsonResponse('Error', 500);
+    }
 
 //    public function delete(int $id)
 //    {
