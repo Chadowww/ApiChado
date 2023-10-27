b207c04f9eeaed2617d7b1d6d65ac3a1dfa4f86c
chado <a.sale@outlook.fr>
2023-10-27 18:27:31 +0200

    Feat: Create service for manage error for request API

---
+++ src/Services/ErrorService.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Services;
+
+use App\Entity\JobOffer;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class ErrorService
+{
+    private ValidatorInterface $validator;
+
+    public function __construct(ValidatorInterface $validator)
+    {
+        $this->validator = $validator;
+    }
+
+    public function getErrors(JobOffer $jobOffer): array
+    {
+       $jobOfferErrors = $this->validator->validate($jobOffer);
+
+         $errors = [];
+        foreach ($jobOfferErrors as $jobOfferError) {
+            if (!isset($errors[$jobOfferError->getPropertyPath()])){
+                $errors['errors'][] = [
+                    'field' => $jobOfferError->getPropertyPath(),
+                    'message' => $jobOfferError->getMessage()
+                ];
+            }
+        }
+        if ($errors) {
+           return $errors;
+        }
+        return [];
+    }
+}
