4262590b234d83e1bae1a436c7ef8cf92c1579e2
chado <a.sale@outlook.fr>
2023-10-28 15:59:22 +0200

    Feat: Created repository for Contract

---
+++ src/Repository/ContractRepository.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Repository;
+
+class ContractRepository
+{
+    public function create(): bool
+    {
+    }
+
+    public function read(): array
+    {
+    }
+
+    public function update():bool
+    {
+    }
+
+    public function delete():bool
+    {
+    }
+
+    public function list(): array
+    {
+    }
+}
