eb600b353a25ee4b9c1c59145ab6a19d1a1916cc
chado <a.sale@outlook.fr>
2023-10-28 15:57:01 +0200

    Feat: Create controller for Contract entity

---
+++ src/Controller/ContractController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class ContractController extends AbstractController
+{
+    public function index(): JsonResponse
+    {
+    }
+
+    public function create(): JsonResponse
+    {
+    }
+
+    public function read(): JsonResponse
+    {
+    }
+
+    public function update(): JsonResponse
+    {
+    }
+
+    public function delete(): JsonResponse
+    {
+    }
+}
