cdfcc7b933b75aeb5ab5b4780f010a73b2e1786f
chado <a.sale@outlook.fr>
2023-10-28 16:42:37 +0200

    Feat: Create method create for Constract

--- src/Repository/ContractRepository.php
+++ src/Repository/ContractRepository.php
@@ -2,10 +2,31 @@
 
 namespace App\Repository;
 
+use App\Entity\Contract;
+use App\Services\ConnectionDbService;
+
 class ContractRepository
 {
-    public function create(): bool
+    private ConnectionDbService $connectionDbService;
+
+    public function __construct(ConnectionDbService $connectionDbService)
     {
+        $this->connection = $connectionDbService->getconnection();
+    }
+    public function create(Contract $contract): bool
+    {
+        try {
+            $this->connection->beginTransaction();
+            $query = 'INSERT INTO contract (type) VALUES (:type)';
+            $statement = $this->connection->prepare($query);
+            $statement->bindValue(':type', $contract->getType());
+            $statement->execute();
+            $this->connection->commit();
+            return true;
+        } catch (\Exception $e) {
+            $this->connectionDbService->getconnection()->rollBack();
+            return false;
+        }
     }
 
     public function read(): array
