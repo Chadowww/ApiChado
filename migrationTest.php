9733047a6307726d126ddb34dc2cf67ef5e4aaea
chado <a.sale@outlook.fr>
2023-10-29 10:36:58 +0100

    Fix: migration test with new const

--- migrationTest.php
+++ migrationTest.php
@@ -11,21 +11,18 @@ require 'config/config.php';
 
 try {
     $pdo = new PDO(
-        'mysql:host=' . DB_HOST . '; charset=utf8',
-        DB_USER,
-        DB_PASSWORD
+        'mysql:host=' . DB_HOST . '; charset=utf8', DB_USER, DB_PASSWORD
     );
 
-    $pdo->exec('DROP DATABASE IF EXISTS ' . DB_NAME);
-    $pdo->exec('CREATE DATABASE ' . DB_NAME);
-    $pdo->exec('USE ' . DB_NAME);
+    $pdo->exec('DROP DATABASE IF EXISTS ' . DB_NAME_TEST);
+    $pdo->exec('CREATE DATABASE ' . DB_NAME_TEST);
+    $pdo->exec('USE ' . DB_NAME_TEST);
 
-    if (is_file(DB_DUMP_PATH) && is_readable(DB_DUMP_PATH)) {
-        $sql = file_get_contents(DB_DUMP_PATH);
-        $statement = $pdo->prepare($sql);
-        $statement->execute();
+    if (is_file(DB_DUMP_PATH_TEST) && is_readable(DB_DUMP_PATH_TEST)) {
+        $sql = file_get_contents(DB_DUMP_PATH_TEST);
+        $statement = $pdo->query($sql);
     } else {
-        echo DB_DUMP_PATH . ' file does not exist';
+        echo DB_DUMP_PATH_TEST . ' file does not exist';
     }
 } catch (PDOException $exception) {
     echo $exception->getMessage();
