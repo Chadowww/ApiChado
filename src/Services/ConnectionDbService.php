0780b7669e730315f330eb123fe10907ecf81756
chado <a.sale@outlook.fr>
2023-11-02 08:29:09 +0100

    Feat: Update the way to connect db

--- src/Services/ConnectionDbService.php
+++ src/Services/ConnectionDbService.php
@@ -10,27 +10,14 @@ class ConnectionDbService
 
     public function __construct()
     {
-//        $this->connection = new PDO(
-//            'mysql:host=' . $_ENV['APP_DB_HOST'] . '; dbname=' . $_ENV['APP_DB_NAME'] . '; charset=utf8',
-//            $_ENV['APP_DB_USER'], $_ENV['APP_DB_PASSWORD']
-//        );
-//        $this->connectionTest = new PDO(
-//            'mysql:host=' . $_ENV['APP_DB_HOST'] . '; dbname=' . $_ENV['APP_DB_NAME_TEST'] . '; charset=utf8',
-//            $_ENV['APP_DB_USER'], $_ENV['APP_DB_PASSWORD']
-//        );
-//        $this->connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        $this->connection = new PDO(
+            'mysql:host=' . $_ENV['APP_DB_HOST'] . '; dbname=' . $_ENV['APP_DB_NAME'] . '; charset=utf8',
+            $_ENV['APP_DB_USER'], $_ENV['APP_DB_PASSWORD']
+        );
     }
 
-    public function connection(string $host, string $dbName, string $dbUser, string $dbPassword): PDO
+    public function connection(): PDO
     {
-         $this->connection = new PDO(
-            'mysql:host=' . $host .
-            '; dbname=' . $dbName .
-            '; charset=utf8', $dbUser, $dbPassword
-        );
-        $this->connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-
         return $this->connection;
-
     }
 }
