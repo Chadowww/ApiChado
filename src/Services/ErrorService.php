aaaec47cbe83b85e51b408387574bc3d7c8e8612
chado <a.sale@outlook.fr>
2023-11-04 19:51:53 +0100

    Feat: Update management error for request of job offer

--- src/Services/ErrorService.php
+++ src/Services/ErrorService.php
@@ -20,42 +20,53 @@ class ErrorService
     {
         $errors = [];
         $data = $request->isMethod('put') ? $request->query->all() : $request->request->all();
-
-        foreach ($data as $key => $value) {
-            if (($key === 'title' || $key === 'description' || $key === 'city') && empty($value)) {
-                $errors['errors'][] = [
-                    'field' => $key,
-                    'message' => 'This value should not be blank.'
+        switch ($data){
+            case $data === []:
+                $errors[] = [
+                    'field' => 'body',
+                    'message' => 'Request body is empty'
                 ];
-            }
-
-            if (($key === 'salaryMin' || $key === 'salaryMax') && !is_numeric($value)) {
-                $errors['errors'][] = [
-                    'field' => $key,
-                    'message' => 'This value should be numeric.'
+                break;
+            case $this->validator->validatePropertyValue(JobOffer::class, 'title', $data['title'])->count() > 0:
+                $errors[] = [
+                    'field' => 'title',
+                    'message' => $this->validator->validatePropertyValue(JobOffer::class, 'title', $data['title'])
+                        ->get(0)->getMessage(),
+                    'passedValue' => $data['title']
                 ];
-            }
+                break;
+            case $this->validator->validatePropertyValue(JobOffer::class, 'description', $data['description'])->count() > 0:
+                $errors[] = [
+                    'field' => 'description',
+                    'message' => $this->validator->validatePropertyValue(JobOffer::class, 'description', $data['description'])->get(0)->getMessage(),
+                    'passedValue' => $data['description']
+                ];
+                break;
+            case $this->validator->validatePropertyValue(JobOffer::class, 'city', $data['city'])->count() > 0:
+                $errors[] = [
+                    'field' => 'city',
+                    'message' => $this->validator->validatePropertyValue(JobOffer::class, 'city', $data['city'])->get
+                    (0)->getMessage(),
+                    'passedValue' => $data['city']
+                ];
+                break;
+            case $this->validator->validatePropertyValue(JobOffer::class, 'salaryMin', (int)$data['salaryMin'])->count() > 0:
+                $errors[] = [
+                    'field' => 'salaryMin',
+                    'message' => $this->validator->validatePropertyValue(JobOffer::class, 'salaryMin', $data['salaryMin'])->get(0)->getMessage(),
+                    'passedValue' => $data['salaryMin']
+                ];
+                break;
+            case $this->validator->validatePropertyValue(JobOffer::class, 'salaryMax', (int)$data['salaryMax'])->count() > 0:
+                $errors[] = [
+                    'field' => 'salaryMax',
+                    'message' => $this->validator->validatePropertyValue(JobOffer::class, 'salaryMax', $data['salaryMax'])->get(0)->getMessage(),
+                    'passedValue' => $data['salaryMax']
+                ];
+                break;
         }
 
         return $errors;
-    }
-    public function getErrorsJobOffer(JobOffer $jobOffer): array
-    {
-       $jobOfferErrors = $this->validator->validate($jobOffer);
-
-         $errors = [];
-        foreach ($jobOfferErrors as $jobOfferError) {
-            if (!isset($errors[$jobOfferError->getPropertyPath()])){
-                $errors['errors'][] = [
-                    'field' => $jobOfferError->getPropertyPath(),
-                    'message' => $jobOfferError->getMessage()
-                ];
-            }
-        }
-        if ($errors) {
-           return $errors;
-        }
-        return [];
     }
 
     Public function getErrorsContract(Contract $contract): array
