1c5f0da9065b5687ffdfb06e617273d34734da2c
chado <a.sale@outlook.fr>
2023-11-05 09:01:55 +0100

    Feat: Update the way to manage errors

--- src/Services/ErrorService.php
+++ src/Services/ErrorService.php
@@ -20,50 +20,63 @@ class ErrorService
     {
         $errors = [];
         $data = $request->isMethod('put') ? $request->query->all() : $request->request->all();
-        switch ($data){
-            case $data === []:
-                $errors[] = [
-                    'field' => 'body',
-                    'message' => 'Request body is empty'
-                ];
-                break;
-            case $this->validator->validatePropertyValue(JobOffer::class, 'title', $data['title'])->count() > 0:
-                $errors[] = [
-                    'field' => 'title',
-                    'message' => $this->validator->validatePropertyValue(JobOffer::class, 'title', $data['title'])
-                        ->get(0)->getMessage(),
-                    'passedValue' => $data['title']
-                ];
-                break;
-            case $this->validator->validatePropertyValue(JobOffer::class, 'description', $data['description'])->count() > 0:
-                $errors[] = [
-                    'field' => 'description',
-                    'message' => $this->validator->validatePropertyValue(JobOffer::class, 'description', $data['description'])->get(0)->getMessage(),
-                    'passedValue' => $data['description']
-                ];
-                break;
-            case $this->validator->validatePropertyValue(JobOffer::class, 'city', $data['city'])->count() > 0:
-                $errors[] = [
-                    'field' => 'city',
-                    'message' => $this->validator->validatePropertyValue(JobOffer::class, 'city', $data['city'])->get
-                    (0)->getMessage(),
-                    'passedValue' => $data['city']
-                ];
-                break;
-            case $this->validator->validatePropertyValue(JobOffer::class, 'salaryMin', (int)$data['salaryMin'])->count() > 0:
-                $errors[] = [
-                    'field' => 'salaryMin',
-                    'message' => $this->validator->validatePropertyValue(JobOffer::class, 'salaryMin', $data['salaryMin'])->get(0)->getMessage(),
-                    'passedValue' => $data['salaryMin']
-                ];
-                break;
-            case $this->validator->validatePropertyValue(JobOffer::class, 'salaryMax', (int)$data['salaryMax'])->count() > 0:
-                $errors[] = [
-                    'field' => 'salaryMax',
-                    'message' => $this->validator->validatePropertyValue(JobOffer::class, 'salaryMax', $data['salaryMax'])->get(0)->getMessage(),
-                    'passedValue' => $data['salaryMax']
-                ];
-                break;
+
+        if (!isset($data['title'], $data['description'], $data['city'], $data['salaryMin'], $data['salaryMax'])) {
+            $errors[] = [
+                'field' => 'request',
+                'message' => 'Request must contain title, description, city, salaryMin and salaryMax fields',
+            ];
+        }
+        foreach ($data as $key => $value) {
+            switch ($key) {
+                case 'title':
+                    if ($this->validator->validatePropertyValue(JobOffer::class, 'title', $value)->count() > 0) {
+                        $errors[] = [
+                            'field' => 'title',
+                            'message' => $this->validator->validatePropertyValue(JobOffer::class, 'title', $value)
+                                ->get(0)->getMessage(),
+                            'passedValue' => $value
+                        ];
+                    }
+                    break;
+                case 'description':
+                    if ($this->validator->validatePropertyValue(JobOffer::class, 'description', $value)->count() > 0) {
+                        $errors[] = [
+                            'field' => 'description',
+                            'message' => $this->validator->validatePropertyValue(JobOffer::class, 'description', $value)->get(0)->getMessage(),
+                            'passedValue' => $value
+                        ];
+                    }
+                    break;
+                case 'city':
+                    if ($this->validator->validatePropertyValue(JobOffer::class, 'city', $value)->count() > 0) {
+                        $errors[] = [
+                            'field' => 'city',
+                            'message' => $this->validator->validatePropertyValue(JobOffer::class, 'city', $value)->get
+                            (0)->getMessage(),
+                            'passedValue' => $value
+                        ];
+                    }
+                    break;
+                case 'salaryMin':
+                    if ($this->validator->validatePropertyValue(JobOffer::class, 'salaryMin', (int)$value)->count() > 0) {
+                        $errors[] = [
+                            'field' => 'salaryMin',
+                            'message' => $this->validator->validatePropertyValue(JobOffer::class, 'salaryMin', $value)->get(0)->getMessage(),
+                            'passedValue' => $value
+                        ];
+                    }
+                    break;
+                case 'salaryMax':
+                    if ($this->validator->validatePropertyValue(JobOffer::class, 'salaryMax', (int)$value)->count() > 0) {
+                        $errors[] = [
+                            'field' => 'salaryMax',
+                            'message' => $this->validator->validatePropertyValue(JobOffer::class, 'salaryMax', $value)->get(0)->getMessage(),
+                            'passedValue' => $value
+                        ];
+                    }
+                    break;
+            }
         }
 
         return $errors;
