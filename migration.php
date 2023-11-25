99521432de1c773db9c00ccefb2cadb1f22b1951
chado <a.sale@outlook.fr>
2023-11-25 09:17:05 +0100

    Feat: Delete unless code

--- migration.php
+++ migration.php
@@ -1,28 +1,25 @@
 <?php
+use Symfony\Component\Dotenv\Dotenv;
 
 require 'vendor/autoload.php';
-if (file_exists('config/db.php')) {
-    require 'config/db.php';
-} else {
-    require 'config/db.php.dist';
-}
 
-require 'config/config.php';
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__.'/.env.local');
 
 try {
     $pdo = new PDO(
-        'mysql:host=' . DB_HOST . '; charset=utf8', DB_USER, DB_PASSWORD
+        'mysql:host=' . $_ENV['APP_DB_HOST'] . '; charset=utf8', $_ENV['APP_DB_USER'], $_ENV['APP_DB_PASSWORD']
     );
 
-    $pdo->exec('DROP DATABASE IF EXISTS ' . DB_NAME);
-    $pdo->exec('CREATE DATABASE ' . DB_NAME);
-    $pdo->exec('USE ' . DB_NAME);
-
-    if (is_file(DB_DUMP_PATH) && is_readable(DB_DUMP_PATH)) {
-        $sql = file_get_contents(DB_DUMP_PATH);
+    $pdo->exec('DROP DATABASE IF EXISTS ' . $_ENV['APP_DB_NAME']);
+    $pdo->exec('CREATE DATABASE ' . $_ENV['APP_DB_NAME']);
+    $pdo->exec('USE ' . $_ENV['APP_DB_NAME']);
+    if (is_file($_ENV['APP_DB_DUMP_PATH']) && is_readable($_ENV['APP_DB_DUMP_PATH'])) {
+        $sql = file_get_contents($_ENV['APP_DB_DUMP_PATH']);
+        dump($sql);
         $statement = $pdo->query($sql);
     } else {
-        echo DB_DUMP_PATH . ' file does not exist';
+        echo $_ENV['APP_DB_DUMP_PATH'] . ' file does not exist';
     }
 } catch (PDOException $exception) {
     echo $exception->getMessage();
