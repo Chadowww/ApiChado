f7e20cabf181c272fa0ded91385068b4928ba258
chado <a.sale@outlook.fr>
2023-12-18 17:23:49 +0100

    Feat: Update the way to manage the roles and update private props for protected

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,7 +7,14 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 class User
 {
-    private ?int $id = null;
+    public const ROLES = [
+        'ROLE_USER' => 1,
+        'ROLE_CANDIDATE' => 2,
+        'ROLE_COMPANY' => 4,
+        'ROLE_ADMIN' => 8,
+    ];
+
+    protected ?int $id = null;
 
     #[Assert\NotBlank(message: "Email is required")]
     #[Assert\Email(message: "Email must be a valid email")]
@@ -22,21 +29,23 @@ class User
         message: "Email must be a valid email"
     )]
     #[Assert\Unique(message: "An account already exists with this email")]
-    private ?string $email = null;
+    protected ?string $email = null;
 
     #[Assert\NotBlank(message: "Password is required")]
     #[Assert\Regex(
         pattern: '/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/',
         message: 'Le mot de passe doit contenir au moins 8 caractères, une majuscule, une minuscule, un chiffre et un caractère spécial ',
         match: true)]
-    private ?string $password = null;
+    protected ?string $password = null;
 
-    private ?array $roles = [];
+    protected ?int $roles = null;
 
-    private bool $is_verified = false;
+    protected bool $is_verified = false;
 
-    private ?DateTime $created_at = null;
-    private ?DateTime $updated_at = null;
+    protected ?DateTime $created_at = null;
+
+    protected ?DateTime $updated_at = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -67,11 +76,25 @@ class User
         $this->password = $password;
     }
 
+    public function hasRole(string $role): bool
+    {
+        return ($this->roles & self::ROLES[$role]) === self::ROLES[$role];
+    }
     public function getRoles(): ?array
+    {
+        $roles = [];
+        foreach (self::ROLES as $role => $value) {
+            if (($this->hasRole($role) & $value) === $value) {
+                $roles[] = $role;
+            }
+        }
+        return $roles;
+    }
+
+    public function getRolesInt(): ?int
     {
         return $this->roles;
     }
-
     public function setRoles(?array $roles): void
     {
         $this->roles = $roles;
@@ -85,5 +108,25 @@ class User
     public function setIsVerified(bool $is_verified): void
     {
         $this->is_verified = $is_verified;
+    }
+
+    public function getCreatedAt(): ?DateTime
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(?DateTime $created_at): void
+    {
+        $this->created_at = $created_at;
+    }
+
+    public function getUpdatedAt(): ?DateTime
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(?DateTime $updated_at): void
+    {
+        $this->updated_at = $updated_at;
     }
 }
