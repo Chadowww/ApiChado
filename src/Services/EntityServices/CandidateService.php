582721134ae0de33efb6e60684f4a6e2ff28a579
chado <a.sale@outlook.fr>
2023-12-20 12:46:38 +0100

    Feat: Add new method to update candidate && refactor code

--- src/Services/EntityServices/CandidateService.php
+++ src/Services/EntityServices/CandidateService.php
@@ -7,14 +7,29 @@ use Symfony\Component\HttpFoundation\Request;
 
 class CandidateService
 {
-
     /**
      * @throws \JsonException
      */
     public function createCandidate(Request $request): Candidate
     {
         $data = json_decode($request->getContent(), true, 512, JSON_THROW_ON_ERROR);
+
         $candidate = new Candidate();
+        return $this->setCandidateData($candidate, $data);
+    }
+
+    /**
+     * @throws \JsonException
+     */
+    public function updateCandidate(Candidate $candidate, Request $request): Candidate
+    {
+        $data = json_decode($request->getContent(), true, 512, JSON_THROW_ON_ERROR);
+
+        return $this->setCandidateData($candidate, $data);
+    }
+
+    private function setCandidateData(Candidate $candidate, array $data): Candidate
+    {
         $candidate->setFirstName($data['firstname']);
         $candidate->setLastName($data['lastname']);
         $candidate->setUserId($data['user_id']);
