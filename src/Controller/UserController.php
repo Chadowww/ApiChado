ca64e275c371252c0acd8d14c1cde742eaeda452
chado <a.sale@outlook.fr>
2023-12-31 12:42:29 +0100

    Feat: Add new validation for unique email

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -93,17 +93,18 @@ class UserController extends AbstractController
      */
     public function create(Request $request): JsonResponse
     {
+        if ($this->userRepository->findByEmail($request)) {
+            throw new InvalidRequestException(json_encode(['error' => 'This email is already used'], JSON_THROW_ON_ERROR), 400);
+        }
         if ($this->errorService->getErrorsUserRequest($request) !== []) {
             throw new InvalidRequestException(json_encode($this->errorService->getErrorsUserRequest($request), JSON_THROW_ON_ERROR), 400);
         }
         $user = $this->UserService->buildUser($request);
-
         try {
             $this->userRepository->create($user);
         } catch (PDOException $e) {
             throw new DatabaseException($this->json(['error' => $e->getMessage()]), 500);
         }
-
         $lastId = $this->userRepository->getLastId();
         return new JsonResponse([
             '201' => 'new user created',
