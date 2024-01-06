ab6e0bba32adf7b23f82fa932700953deb910dd3
chado <a.sale@outlook.fr>
2024-01-06 12:56:32 +0100

    Commit message:
    Add CandidateTest for testing candidate creation
    
    Introduced a new test file, CandidateTest, to test our CandidateController and CandidateService. This includes setting up mocks and a specific test method for checking the "create" operation. The test ensures that new candidate creation correctly returns a status code of 201.

---
+++ tests/CandidateTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Tests;
+
+use App\Controller\CandidateController;
+use App\Exceptions\DatabaseException;
+use App\Exceptions\InvalidRequestException;
+use App\Repository\CandidateRepository;
+use App\Services\EntityServices\CandidateService;
+use App\Services\ErrorService;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\ConstraintViolation;
+use Symfony\Component\Validator\ConstraintViolationList;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class CandidateTest extends TestCase
+{
+    private CandidateService $candidateService;
+    private SerializerInterface $serializer;
+    private ValidatorInterface $validator;
+    private ErrorService $errorService;
+    private CandidateRepository $mockRepository;
+    private CandidateController $mockController;
+
+    public function __construct(?string $name = null, array $data = [], $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+        $this->candidateService = $this->createMock(CandidateService::class);
+        $this->serializer = $this->createMock(SerializerInterface::class);
+        $this->validator = $this->createMock(ValidatorInterface::class);
+        $this->errorService = $this->createMock(ErrorService::class);
+        $this->mockRepository = $this->createMock(CandidateRepository::class);
+        $this->mockController = new CandidateController(
+            $this->errorService,
+            $this->candidateService,
+            $this->mockRepository,
+            $this->serializer,
+        );
+    }
+
+    public function testCandidateCreate(): void
+    {
+        $data = [
+            'firstname' => 'John',
+            'lastname' => 'Doe',
+            'phone' => '1234567890',
+            'address' => '123 Main St',
+            'city' => 'New York',
+            'country' => 'USA',
+            'user_id' => '1',
+        ];
+        $request = new Request([], [], [], [], [], [], json_encode($data));
+        $request->setMethod('POST');
+        $request->headers->set('Content-Type', 'application/json');
+        $response = $this->mockController->create($request);
+        $this->assertEquals(201, $response->getStatusCode());
+    }
+}
