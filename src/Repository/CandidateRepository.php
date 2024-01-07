499f0fe53dc183523192d2920bc4dafcbd5df72f
chado <a.sale@outlook.fr>
2024-01-07 09:29:28 +0100

    Refactor CandidateRepository to improve database operations
    The CandidateRepository methods have been refactored to improve database operations. A BindValueService has been introduced to handle the binding of values to statements, reducing code repetition. Also, common database operations now use a general executeTransaction method to streamline the process and improve error handling.

--- src/Repository/CandidateRepository.php
+++ src/Repository/CandidateRepository.php
@@ -4,46 +4,55 @@ namespace App\Repository;
 
 use App\Entity\Candidate;
 use App\Services\ConnectionDbService;
+use App\Services\DataBaseServices\BindValueService;
 use PDO;
 use PDOException;
 
 class CandidateRepository
 {
     private PDO $connection;
+    private BindValueService $bindValueService;
 
-    public function __construct(ConnectionDbService $connection)
+    CONST array VALUES = [
+        'firstname' => ':firstname',
+        'lastname' => ':lastname',
+        'phone' => ':phone',
+        'address' => ':address',
+        'city' => ':city',
+        'country' => ':country',
+        'avatar' => ':avatar',
+        'slug' => ':slug',
+        'coverLetter' => ':coverLetter',
+        'user_id' => ':user_id',
+    ];
+
+    public function __construct(ConnectionDbService $connection, BindValueService $bindValueService)
     {
         $this->connection = $connection->connection();
+        $this->bindValueService = $bindValueService;
     }
 
     public function create(Candidate $candidate): bool
     {
-        $this->connection->beginTransaction();
+        $candidateAttributes = [];
 
-        try {
+        $this->executeTransaction(function () use ($candidate, &$candidateAttributes) {
             $query = '
             INSERT INTO APICHADO.candidate
             (firstname, lastname, phone, address, city, country, avatar, slug, coverLetter, user_id) 
             VALUES 
             (:firstname, :lastname, :phone, :address, :city, :country, :avatar, :slug, :coverLetter, :user_id)';
+
             $statement = $this->connection->prepare($query);
-            $statement->bindValue(':firstname', $candidate->getFirstname());
-            $statement->bindValue(':lastname', $candidate->getLastname());
-            $statement->bindValue(':phone', $candidate->getPhone());
-            $statement->bindValue(':address', $candidate->getAddress());
-            $statement->bindValue(':city', $candidate->getCity());
-            $statement->bindValue(':country', $candidate->getCountry());
-            $statement->bindValue(':avatar', $candidate->getAvatar());
-            $statement->bindValue(':slug', $candidate->getSlug());
-            $statement->bindValue(':coverLetter', $candidate->getCoverLetter());
-            $statement->bindValue(':user_id', $candidate->getUserId());
+
+            foreach (self::VALUES as $key => $value) {
+                $candidateAttributes[$value] = $candidate->{"get" . ucfirst($key)}();
+            }
+            $this->bindValueService->bindValuesToStatement($statement, $candidateAttributes);
             $statement->execute();
-            $this->connection->commit();
-            return true;
-        } catch (PDOException $e) {
-            $this->connection->rollBack();
-            throw $e;
-        }
+        });
+
+        return true;
     }
 
     public function read(int $id) : Candidate | bool
@@ -61,66 +70,49 @@ class CandidateRepository
 
     public function update(Candidate $candidate): bool
     {
-        error_log($candidate->getId());
-        try {
-            $this->connection->beginTransaction();
+        $candidateAttributes = [];
+
+        $this->executeTransaction(function () use ($candidate, &$candidateAttributes){
             $query = '
             UPDATE APICHADO.candidate
             SET firstname = :firstname, lastname = :lastname, phone = :phone, address = :address, city = :city, country = :country, avatar = :avatar, slug = :slug, user_id = :user_id
             WHERE user_id = :user_id';
+
             $statement = $this->connection->prepare($query);
-            $statement->bindValue(':firstname', $candidate->getFirstname());
-            $statement->bindValue(':lastname', $candidate->getLastname());
-            $statement->bindValue(':phone', $candidate->getPhone());
-            $statement->bindValue(':address', $candidate->getAddress());
-            $statement->bindValue(':city', $candidate->getCity());
-            $statement->bindValue(':country', $candidate->getCountry());
-            $statement->bindValue(':avatar', $candidate->getAvatar());
-            $statement->bindValue(':slug', $candidate->getSlug());
-            $statement->bindValue(':user_id', $candidate->getUserId());
+
+            foreach (self::VALUES as $key => $value) {
+                $candidateAttributes[$value] = $candidate->{"get" . ucfirst($key)}();
+            }
+            $this->bindValueService->bindValuesToStatement($statement, $candidateAttributes);
+
             $statement->execute();
-            $this->connection->commit();
-            error_log('update candidate');
-            return true;
-        } catch (PDOException $e) {
-            error_log('error in update candidate');
-            $this->connection->rollBack();
-            throw $e;
-        }
+        });
+
+        return true;
     }
 
     public function delete(int $id): bool
     {
-        try {
-            $this->connection->beginTransaction();
+        $this->executeTransaction(function () use ($id) {
             $query = 'DELETE FROM APICHADO.candidate WHERE id = :id';
             $statement = $this->connection->prepare($query);
             $statement->bindValue(':id', $id);
             $statement->execute();
-            $this->connection->commit();
-            return true;
-        } catch (PDOException $e) {
-            $this->connection->rollBack();
-            throw $e;
-        }
+        });
+        return true;
     }
 
-    public function list(){
-        try {
-            $this->connection->beginTransaction();
+    public function list(): array
+    {
+        $this->executeTransaction(function () use (&$candidates) {
             $query = 'SELECT c.*, u.* FROM APICHADO.candidate c LEFT JOIN APICHADO.user u ON c.user_id = u.id';
             $statement = $this->connection->query($query);
-            $candidates = $statement->fetchObject(Candidate::class);
-            $this->connection->commit();
-
-            return $candidates;
-        } catch (PDOException $e) {
-            $this->connection->rollBack();
-            throw $e;
-        }
+            $candidates = $statement->fetchAll(PDO::FETCH_CLASS, Candidate::class);
+        });
+        return $candidates;
     }
 
-    public function getByUserId($id):Candidate
+    public function getByUserId($id):Candidate | bool
     {
         $this->connection->beginTransaction();
         $query = 'SELECT c.*, u.* FROM APICHADO.candidate c LEFT JOIN APICHADO.user u ON c.user_id = u.id WHERE c.user_id = :id';
@@ -131,5 +123,19 @@ class CandidateRepository
         $this->connection->commit();
 
         return $candidate;
+    }
+
+    private function executeTransaction(callable $transaction): Void
+    {
+        try {
+            $this->connection->beginTransaction();
+
+            $transaction();
+
+            $this->connection->commit();
+        } catch (PDOException $e) {
+            $this->connection->rollBack();
+            throw $e;
+        }
     }
 }
