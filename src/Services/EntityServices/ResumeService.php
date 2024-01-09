fb486c0cb19be8ca2717a63d3a41ba45f0ff6555
chado <a.sale@outlook.fr>
2024-01-09 14:59:40 +0100

    Add ResumeService for handling resumes
    A new service class, ResumeService, was added under the 'src/Services/EntityServices' directory. This service will handle the creation of Resume entities, taking necessary data from an HTTP request and assigned the values to a new Resume instance. This includes handling uploaded files and dynamically setting timestamps for 'created_at' and 'updated_at'.

---
+++ src/Services/EntityServices/ResumeService.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Services\EntityServices;
+
+use App\Entity\Resume;
+use Symfony\Component\HttpFoundation\Request;
+
+class ResumeService
+{
+
+    public function createResume(Request $request, string $fileName): Resume
+    {
+        $uploadedFile = $request->files->get('file');
+        $resume = new Resume();
+        $resume->setTitle($request->request->get('title'));
+        $resume->setFilename($fileName);
+        $resume->setCandidateId($request->request->get('candidate_id'));
+        $resume->setCreatedAt(date('Y-m-d H:i:s'));
+        $resume->setUpdatedAt(date('Y-m-d H:i:s'));
+
+        return $resume;
+    }
+}
