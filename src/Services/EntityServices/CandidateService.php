85e2aa00e424b3f606c3a5f15981b211b358febe
chado <a.sale@outlook.fr>
2024-01-12 09:08:46 +0100

    Refactor 'user_id' to 'userId' in CandidateService
    The method 'setUser_Id' in CandidateService has been refactored to 'setUserId'. This follows the camel case naming convention, improving code readability and maintainability. The change has been propagated to the corresponding function call.

--- src/Services/EntityServices/CandidateService.php
+++ src/Services/EntityServices/CandidateService.php
@@ -32,7 +32,7 @@ class CandidateService
     {
         $candidate->setFirstName($data['firstname']);
         $candidate->setLastName($data['lastname']);
-        $candidate->setUser_Id($data['user_id']);
+        $candidate->setUserId($data['user_id']);
         $candidate->setSlug($data['firstname'], $data['lastname']);
         $candidate->setPhone($data['phone'] ?? null);
         $candidate->setAddress($data['address'] ?? null);
