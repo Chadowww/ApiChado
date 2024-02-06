0bf041fcf6c9b92b1176f67d4d08f9304503f773
chado <a.sale@outlook.fr>
2024-02-06 06:24:16 +0100

    Update test bootstrap and composer.lock file
    Removed the secondary .env.test.local boot up in test bootstrap file. Also, updated Symfony's monolog-bridge to version v6.3.12 and added "ext-fileinfo" in the required extensions of composer.lock.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,7 +8,6 @@ if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
     require dirname(__DIR__).'/config/bootstrap.php';
 } elseif (method_exists(Dotenv::class, 'bootEnv')) {
     (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env.test.local');
 }
 
 if ($_SERVER['APP_DEBUG']) {
