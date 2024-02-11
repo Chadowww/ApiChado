709ca08b368b01ea212c5cd3acff98e48863eb79
chado <a.sale@outlook.fr>
2024-02-11 09:18:59 +0100

    Refactor ResumeRepository with TransactionDbService
    Refactored the ResumeRepository to use TransactionDbService for database interactions, improving error handling. Replaced BindValueService by binding values directly in the statement, leading to code simplification. The return of delete method now properly reflects the operation result. These changes enhance code readability and maintainability.

--- src/Repository/ResumeRepository.php
+++ src/Repository/ResumeRepository.php
@@ -3,45 +3,44 @@
 namespace App\Repository;
 
 use App\Entity\Resume;
+use App\Exceptions\DatabaseException;
 use App\Services\ConnectionDbService;
 use App\Services\DataBaseServices\BindValueService;
+use App\Services\DataBaseServices\TransactionDbService;
 use PDO;
 use PDOException;
 
 class ResumeRepository
 {
     private PDO $connection;
-    private BindValueService $bindValueService;
 
-    CONST array VALUES = [
+    const array VALUES = [
         'title' => ':title',
         'filename' => ':filename',
         'candidateId' => ':candidateId',
     ];
+    private TransactionDbService $transactionDbService;
 
-    public function __construct(ConnectionDbService $connection, BindValueService $bindValueService)
+    public function __construct(ConnectionDbService $connection, TransactionDbService$transactionDbService)
     {
         $this->connection = $connection->connection();
-        $this->bindValueService = $bindValueService;
+        $this->transactionDbService = $transactionDbService;
     }
 
+    /**
+     * @throws DatabaseException
+     */
     public function create(Resume $resume): bool
     {
-        $resumeAttributes = [];
-
-        $this->executeTransaction(function () use ($resume, &$resumeAttributes) {
-            $query = '
-            INSERT INTO APICHADO.resume
-            (title, filename, candidateId) 
-            VALUES 
-            (:title, :filename, :candidateId)';
+        $this->transactionDbService->executeTransaction(function () use ($resume) {
+            $query = 'INSERT INTO resume (title, filename, candidateId) VALUES (:title, :filename, :candidateId)';
 
             $statement = $this->connection->prepare($query);
 
             foreach (self::VALUES as $key => $value) {
-                $resumeAttributes[$value] = $resume->{"get" . ucfirst($key)}();
+                $method = "get" . ucfirst($key);
+                $statement->bindValue($value, $resume->$method());
             }
-            $this->bindValueService->bindValuesToStatement($statement, $resumeAttributes);
 
             $statement->execute();
         });
@@ -49,83 +48,89 @@ class ResumeRepository
         return true;
     }
 
-    public function read(int $resumeId): Resume | bool
+    /**
+     * @throws DatabaseException
+     */
+    public function read(int $resumeId): ?Resume
     {
-        $this->connection->beginTransaction();
-        $query = 'SELECT * FROM APICHADO.resume WHERE resumeId = :resumeId';
-        $statement = $this->connection->prepare($query);
-        $statement->bindValue(':resumeId', $resumeId, PDO::PARAM_INT);
-        $statement->execute();
-        $resume = $statement->fetchObject(Resume::class);
-        $this->connection->commit();
+        $this->transactionDbService->executeTransaction(function () use ($resumeId, &$resume) {
+            $query = 'SELECT * FROM resume WHERE resumeId = :resumeId';
+            $statement = $this->connection->prepare($query);
+            $statement->bindValue(':resumeId', $resumeId, PDO::PARAM_INT);
+            $statement->execute();
+            $resume = $statement->fetchObject(Resume::class);
+        });
         return $resume;
     }
 
+    /**
+     * @throws DatabaseException
+     */
     public function update(Resume $resume): bool
     {
-        $resumeAttributes = [];
-
-        $this->executeTransaction(function () use ($resume, &$resumeAttributes) {
+        $this->transactionDbService->executeTransaction(function () use ($resume) {
             $query = '
-            UPDATE APICHADO.resume
+            UPDATE resume
             SET title = :title, filename = :filename, candidateId = :candidateId
             WHERE resumeId = :resumeId';
 
             $statement = $this->connection->prepare($query);
 
             foreach (self::VALUES as $key => $value) {
-                $resumeAttributes[$value] = $resume->{"get" . ucfirst($key)}();
+                $method = "get" . ucfirst($key);
+                $statement->bindValue($value, $resume->$method());
             }
-            $resumeAttributes[':resumeId'] = $resume->getResumeId();
-            $this->bindValueService->bindValuesToStatement($statement, $resumeAttributes);
+            $statement->bindValue(':resumeId', $resume->getResumeId(), PDO::PARAM_INT);
+
             $statement->execute();
         });
 
         return true;
     }
+
+    /**
+     * @throws DatabaseException
+     */
     public function delete(int $id): bool
     {
-        $this->executeTransaction(function () use ($id) {
-            $query = 'DELETE FROM APICHADO.resume WHERE resumeId = :id';
+        $this->transactionDbService->executeTransaction(function () use ($id, &$statement) {
+            $query = 'DELETE FROM resume WHERE resumeId = :id';
             $statement = $this->connection->prepare($query);
-            $statement->bindValue(':id', $id, PDO::PARAM_STR);
+            $statement->bindValue(':id', $id, PDO::PARAM_INT);
             $statement->execute();
         });
-        return true;
+
+        return $statement->rowCount() > 0;
     }
+
+    /**
+     * @throws DatabaseException
+     */
     public function list(): array
     {
-        $this->connection->beginTransaction();
-        $query = 'SELECT * FROM APICHADO.resume';
-        $statement = $this->connection->query($query);
-        $resumes = $statement->fetchAll(PDO::FETCH_CLASS, Resume::class);
-        $this->connection->commit();
+        $this->transactionDbService->executeTransaction(function () use (&$resumes) {
+            $query = 'SELECT * FROM resume';
+            $statement = $this->connection->query($query);
+            $resumes = $statement->fetchAll(PDO::FETCH_CLASS, Resume::class);
+        });
+
         return $resumes;
     }
 
-    private function executeTransaction(callable $transaction): Void
+    /**
+     * @throws DatabaseException
+     */
+    public function findByCandidate($candidateId): false|array
     {
-        try {
-            $this->connection->beginTransaction();
+        $this->transactionDbService->executeTransaction(function () use ($candidateId, &$resumes) {
+            $query = 'SELECT * FROM resume WHERE candidateId = :candidateId';
+            $statement = $this->connection->prepare($query);
+            $statement->bindValue(':candidateId', $candidateId, PDO::PARAM_INT);
+            $statement->execute();
+            $resumes = $statement->fetchAll(PDO::FETCH_CLASS, Resume::class);
+        });
 
-            $transaction();
-
-            $this->connection->commit();
-        } catch (PDOException $e) {
-            $this->connection->rollBack();
-            throw $e;
-        }
-    }
-
-    public function findByCandidate($resumeId)
-    {
-        $this->connection->beginTransaction();
-        $query = 'SELECT * FROM APICHADO.resume WHERE candidateId = :candidateId';
-        $statement = $this->connection->prepare($query);
-        $statement->bindValue(':candidateId', $resumeId, PDO::PARAM_INT);
-        $statement->execute();
-        $resumes = $statement->fetchAll(PDO::FETCH_CLASS, Resume::class);
-        $this->connection->commit();
         return $resumes;
     }
+
 }
